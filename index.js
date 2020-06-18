54680ca95eeee9e964b7e0e72d4c1f5c092aa160
geraldos <sepdwijaya18@gmail.com>
2020-06-18 12:43:05 +0700

    updated style

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let buttonValue = document.getElementById('button-input')
 
 buttonValue.addEventListener('click', function () {
-    let regexName = /[A-Z]/
-    let inputValue = document.getElementById('input').value
+  let regexName = /[A-Z]/
+  let inputValue = document.getElementById('input').value
 
-    if (inputValue[0].match(regexName)) {
-        document.querySelector('p').innerHTML = 'huruf pertama adalah huruf Besar'
-    } else {
-        document.querySelector('p').innerHTML = 'huruf pertama adalah huruf Kecil'
-    }
+  if (inputValue[0].match(regexName)) {
+    alert('huruf pertama adalah huruf Besar')
+  } else {
+    alert('huruf pertama adalah huruf Kecil')
+  }
 })
